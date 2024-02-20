6c4d6a75bceb3f0d2a3de773155bd520945ad52d
Ulthran <ctbushman@gmail.com>
2024-02-20 13:27:12 -0500

    Add DAFOTPlus tests

--- tests/testthat/test-DAFOTPlus_function.R
+++ tests/testthat/test-DAFOTPlus_function.R
@@ -1,3 +1,35 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("IndDAFOT function works as expected", {
+  # Generate example data
+  Tree <- rtree(100)
+  P <- rbind(matrix(1, nrow = length(Tree$tip.label), ncol = 100),
+             matrix(0, nrow = Tree$Nnode, ncol = 100))
+  Y <- c(rep(1, 50), rep(2, 50))
+  
+  # Test IndDAFOT function
+  result <- IndDAFOT(P, Y, Tree)
+  
+  # Check if the result is a list with components Stat and P
+  expect_type(result, "list")
+  expect_named(result, c("Stat", "P"))
+  expect_length(result$Stat, 2)
+  expect_length(result$P, 2)
+})
+
+### TAKES WAY TOO LONG
+test_that("ConIndDAFOT function works as expected", {
+  # Generate example data
+  Tree <- rtree(100)
+  P <- rbind(matrix(1, nrow = length(Tree$tip.label), ncol = 100),
+             matrix(0, nrow = Tree$Nnode, ncol = 100))
+  Y <- c(rep(1, 50), rep(2, 50))
+  X <- matrix(rnorm(500), nrow = 100)
+  
+  # Test ConIndDAFOT function
+  #result <- ConIndDAFOT(P, Y, X, Tree)
+  
+  # Check if the result is a list with components Stat and P
+  #expect_type(result, "list")
+  #expect_named(result, c("Stat", "P"))
+  #expect_length(result$Stat, 2)
+  #expect_length(result$P, 2)
 })
