1dbc131b27b113b4b460b27765056e2dccafb57e
Ulthran <ctbushman@gmail.com>
2024-02-22 16:41:32 -0500

    Document test_IndDAFOT data

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' A data set designed for testing IndDAFOT function
+#'
+#' @format ## `test_IndDAFOT`
+#' A named list:
+#' \describe{
+#'   \item{P}{Relative abundances}
+#'   \item{Y}{Outcome}
+#'   \item{tree}{Tree}
+#' }
+"test_IndDAFOT"
--- tests/testthat/test-DAFOTPlus_function.R
+++ tests/testthat/test-DAFOTPlus_function.R
@@ -5,6 +5,14 @@ test_that("IndDAFOT function works as expected", {
   expect_true(all(res$P < 0.05))
 })
 
+# ~5s per iteration (step value) on test data
 test_that("ConIndDAFOT function works as expected", {
+  d <- test_ConIndDAFOT
+  #res_condgen <- ConIndDAFOT(d$P, d$Y, d$X, d$tree, condgen = d$condgen, step = 5)
   
+  
+  
+  res_Ex <- ConIndDAFOT(d$P, d$Y, d$X, d$tree, ExY = d$ExY, ExX = d$ExX, Exk = d$Exk, step = 5)
+  
+  expect_true(all(res_Ex$P < 0.05))
 })
